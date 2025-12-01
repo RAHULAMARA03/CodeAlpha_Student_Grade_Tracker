0fc6829aa806df2b2be602cbfc563a2884f1cd03
RAHUL AMARA <rahulssb2003@gmail.com>
2025-12-01 22:54:04 +0530

    updatedcode

--- StudentGradeTracker.java
+++ StudentGradeTracker.java
@@ -1,49 +1,72 @@
-import java.util.Scanner;
+import java.util.*;
+class StudentGradeTracker{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Student> students=new ArrayList<>();
+        int choice;
+        do { 
+           System.out.println("Student Grade Tracker") ;
+           System.out.println("1.Add Student");
+           System.out.println("2.Summary Report");
+           System.out.println("3.Exit");
+           System.out.println("Enter youir choice");
+           choice=sc.nextInt();
+           sc.nextLine();
+           switch(choice){
+            case 1:     System.out.println("Enter the student name:");
+                        String name=sc.nextLine();
+                        System.out.println("Enter the number of subjects");
+                        int n=sc.nextInt();
+                        ArrayList<Integer> grades=new ArrayList<>();
+                        
+                        for(int i=0; i<n; i++){
+                            System.out.println("Enter the grade for subject "+(i+1));
+                            grades.add(sc.nextInt());
+                        }
+                        students.add(new Student(name,grades));
+                        System.out.println("Student added successfully!");
+                        System.out.println();
+                        break;
+                    
+            case 2:     System.out.println("Students Summary Report:");
+                        System.out.println();
+                        for(Student st: students){
+                            System.out.println("Name: "+st.name);
+                            System.out.println("Grades: "+st.grades);
+                            System.out.println("Average Grade: "+st.getAverage());
+                            System.out.println("Highest Grade: "+st.getHighest());
+                            System.out.println("Lowest Grade: "+st.getLowest());
+                             System.out.println();
+                        }
+                        System.out.println();
 
-public class StudentGradeTracker {
+                        break;
+            case 3:     System.out.println("Program Exiting......");
+                        break;
+            default: System.out.println("Invalod choice... Try again");
 
-    public static void main(String[] args) {
-
-        System.out.println("enter no of student grades to be taken : ");
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        System.out.println("enter the student grades : ");
-
-        int[] arr = new int[n];
-
-        for(int i = 0;i < n;i++){
-
-            arr[i] = sc.nextInt();
-
-        }
-
-        int minGrade = Integer.MAX_VALUE;
-
-        int maxGrade = Integer.MIN_VALUE;
-
-        double avg = 0;
-
-        for(int num : arr){
-
-            avg += num;
-
-            minGrade = Math.min(minGrade,num);
-
-            maxGrade = Math.max(maxGrade,num);
-
-        }
-
-        avg /= n;
-
-        System.out.println("average of student grades : "+avg);
-
-        System.out.println("maximum grade given is : "+maxGrade);
-
-        System.out.println("minimum grade given is : "+minGrade);
-
+           }
+        } while (choice!=3);
     }
-
+}
+class Student{
+    String name;
+    ArrayList<Integer> grades;
+    Student(String name,ArrayList<Integer> grades){
+        this.name=name;
+        this.grades=grades;
+    }
+    double getAverage(){
+        double sum=0;
+        for(int grade:grades){
+             sum+=grade;
+        }
+        return (double) sum/grades.size();
+    }
+    int getHighest(){
+        return Collections.max(grades);
+    }
+    int getLowest(){
+        return Collections.min(grades);
+    }
 }
